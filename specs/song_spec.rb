363ddc00fff23766f35d96a1488ee0ebb2fe43c9
Elliot Dixon <elliotjohndixon@gmail.com>
2019-09-20 11:46:53 +0100

    song files setup

---
+++ specs/song_spec.rb
@@ -0,0 +1,17 @@
+require("minitest/autorun")
+require ("minitest/rg")
+require_relative("../song.rb")
+
+class SongTest < MiniTest::Test
+
+  def setup
+    @song1 = Song.new("Kali Ma", "Neck Deep")
+    @song2 = Song.new("See You Again", "Tyler, The Creator")
+  end
+
+  def test_setup
+    assert_equal("Kali Ma", @song1.name)
+    assert_equal("Tyler, The Creator", @song2.artist)
+  end
+
+end
